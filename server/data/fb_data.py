dcf123a45448ca12bc314758d67ea94d2571d300
Lydia <lydiaawood@googlemail.com>
2021-03-11 11:15:02 +0000

    data added

---
+++ server/data/fb_data.py
@@ -0,0 +1,23 @@
+fb_list = [
+    Product(product_name="Heineken", product_type="beverage", in_stock=True, price=6.50),
+    Product(product_name="Carlsberg", product_type="beverage", in_stock=True, price=6.50),
+    Product(product_name="White wine", product_type="beverage", in_stock=True, price=7.10),
+    Product(product_name="Red wine", product_type="beverage", in_stock=True, price=7.10),
+    Product(product_name="Water", product_type="beverage", in_stock=True, price=3.50),
+    Product(product_name="Coca Cola", product_type="beverage", in_stock=True, price=3.35),
+    Product(product_name="Orange juice", product_type="beverage", in_stock=True, price=3.20),
+    Product(product_name="Rose wine", product_type="beverage", in_stock=True, price=7.10),
+    Product(product_name="Vodka", product_type="beverage", in_stock=True, price=6.25),
+    Product(product_name="Whiskey", product_type="beverage", in_stock=True, price=6.25),
+    Product(product_name="Lemonade", product_type="beverage", in_stock=True, price=3.35),
+    Product(product_name="Cheeseburger",product_type="food", price = 11, in_stock=True),
+    Product(product_name="Beetroot Burger",product_type="food", price = 11, in_stock=True),
+    Product(product_name="Grilled Chicken Burrito",product_type="food", price = 9, in_stock=True),
+    Product(product_name="Guacamole Burrito",product_type="food", price = 9, in_stock=True),
+    Product(product_name="Chicken Katsu",product_type="food", price = 14, in_stock=True),
+    Product(product_name="Pizza Margherita",product_type="food", price = 8, in_stock=True),
+    Product(product_name="BBQ Pork Ribs",product_type="food", price = 12, in_stock=True),
+    Product(product_name="Vegan Pizza Margherita",product_type="food", price = 8, in_stock=True),
+    Product(product_name="Vegetable Samosas",product_type="food", price = 10, in_stock=True),
+    Product(product_name="Falafel Wrap",product_type="food", price = 10, in_stock=True)
+]
---
+++ server/data/user_data.py
@@ -0,0 +1,6 @@
+user_list = [
+    User(user_name="fabien", email="fab@fab.com", password="fabfabfab1", password_confirmation="fabfabfab1", is_admin=True),
+    User(user_name="lydia", email="lydia@lydia.com", password="lydialydia1", password_confirmation="lydialydia1", is_admin=True),
+    User(user_name="kate", email="kate@kate.com", password="katekate1", password_confirmation="katekate1", is_admin=True),
+    User(user_name="test", email="test@test.com", password="testtest1", password_confirmation="testtest1", is_admin=False),
+]
